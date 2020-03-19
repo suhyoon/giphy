ba3885b6618b25a9b2a15002d06cbfc45fa709a4
suhyoon <suh@scripted.org>
2020-03-19 14:43:15 +0000

    changes

--- js/script.js
+++ js/script.js
@@ -4,8 +4,18 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
-});
+     $(".gallery").empty(); 
+    let input = $("#search-term").val();
+    let imgUrl = "https://api.giphy.com/v1/gifs/search?q="+input+"&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY";
+//https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC 
+    fetch(imgUrl)
+    .then(function(response) {
+       return response.json();
+    })
+    .then(function(data) {
+       
+       $(".gallery").append("<img src=' + data[2].imgUrl + '/>");
+       
 
+    });
+});
